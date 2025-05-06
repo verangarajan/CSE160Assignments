ff094ce16f0dfccef62cdbf9b65b304262e25c7e
verangarajan <verangar@ucsc.edu>
2025-05-06 16:41:31 -0700

    initial commit

---
+++ ASG3/BlockyAnimal.js
@@ -0,0 +1,469 @@
+// ColoredPoint.js (c) 2012 matsuda
+// Vertex shader program
+
+//Code adapted from Prof. Davis videos on Youtube
+//Used online documentation and ChatGPT for debugging purposes
+
+var VSHADER_SOURCE = `
+  precision mediump float;
+  attribute vec4 a_Position;
+  attribute vec2 a_UV;
+  varying vec2 v_UV;
+  uniform mat4 u_ModelMatrix;
+  uniform mat4 u_GlobalRotateMatrix;
+  uniform mat4 u_viewMatrix;
+  uniform mat4 u_ProjectionMatrix;
+  void main() {
+    gl_Position = u_ProjectionMatrix * u_viewMatrix * u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+    v_UV = a_UV;
+  }
+`
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+  precision mediump float;
+  varying vec2 v_UV;
+  uniform vec4 u_FragColor;
+  void main() {
+    gl_FragColor = u_FragColor;
+  }
+`
+
+let canvas;
+let gl;
+let a_Position;
+let u_FragColor;
+let u_Size;
+let a_UV;
+let u_GlobalRotateMatrix;
+let u_ModelMatrix;
+let u_ProjectionMatrix;
+let u_viewMatrix;
+function setupWebGL()
+{
+ // Retrieve <canvas> element
+ canvas = document.getElementById('webgl');
+
+ // Get the rendering context for WebGL
+ //gl = getWebGLContext(canvas);
+gl = canvas.getContext("webgl", {preserveDrawingBuffer: true});
+
+ if (!gl) {
+   console.log('Failed to get the rendering context for WebGL');
+   return;
+ }
+
+ gl.enable(gl.DEPTH_TEST);
+
+}
+
+function connectVariablesToGLSL()
+{
+// Initialize shaders
+if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+  console.log('Failed to intialize shaders.');
+  return;
+}
+
+// // Get the storage location of a_Position
+ a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+if (a_Position < 0) {
+  console.log('Failed to get the storage location of a_Position');
+  return;
+}
+
+// Get the storage location of u_FragColor
+ u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+if (!u_FragColor) {
+  console.log('Failed to get the storage location of u_FragColor');
+  return;
+}
+
+a_UV = gl.getAttribLocation(gl.program, 'a_UV');
+if (a_UV < 0) {
+  console.log('Failed to get the storage location of a_UV');
+  return;
+}
+
+u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+if (!u_ModelMatrix) {
+  console.log('Failed to get the storage location of u_ModelMatrix');
+  return;
+}
+
+u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
+if (!u_GlobalRotateMatrix) {
+  console.log('Failed to get the storage location of u_GlobalRotateMatrix');
+  return;
+
+}
+
+u_viewMatrix = gl.getUniformLocation(gl.program, 'u_viewMatrix');
+if (!u_viewMatrix) {
+  console.log('Failed to get the storage location of u_viewMatrix');
+  return;
+}
+
+u_ProjectionMatrix = gl.getUniformLocation(gl.program, 'u_ProjectionMatrix');
+if (!u_ProjectionMatrix) {
+  console.log('Failed to get the storage location of u_ProjectionMatrix');
+  return;
+}
+
+
+var identityM = new Matrix4();
+gl.uniformMatrix4fv(u_ModelMatrix, false, identityM.elements);
+
+var viewM = new Matrix4();
+gl.uniformMatrix4fv(u_viewMatrix, false, viewM.elements);
+
+var projectionM = new Matrix4();
+gl.uniformMatrix4fv(u_ProjectionMatrix, false, projectionM.elements);
+
+
+//get storage location of u_Size
+//u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+//if (!u_Size) {
+ // console.log('Failed to get the storage location of u_Size ');
+  //return;
+//}
+
+}
+
+//Globals related to UI elements
+const POINT = 0;
+const TRIANGLE = 1;
+const CIRCLE = 2;
+
+let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let g_selectedSize = 5;
+let g_segmentSize = 26;
+let g_selectedType = POINT;
+let g_globalAngle = 0;
+let g_rightArmAngle = 90;
+let g_rightHandAngle = 90;
+let g_rightFingerAngle = 90;
+let g_armAnimation = false;
+function addActionsForHtmlUI()
+{
+
+  document.getElementById("animationArmONbutton").onclick = function() {g_armAnimation = true;};
+  document.getElementById("animationArmOFFbutton").onclick = function() {g_armAnimation = false;};
+
+
+
+  document.getElementById('red').onclick = function() {g_selectedColor = [1.0, 0.0, 0.0, 1.0];};
+  document.getElementById('green').onclick = function() {g_selectedColor = [0.0, 1.0, 0.0, 1.0];};
+
+  //clear button
+  document.getElementById('clear').onclick = function() {g_shapesList = []; renderAllShapes();};
+
+  //point and triangle buttons
+  document.getElementById('pointButton').onclick = function() {g_selectedType = POINT};
+  document.getElementById('triButton').onclick = function() {g_selectedType = TRIANGLE};
+  document.getElementById('circleButton').onclick = function() {g_selectedType = CIRCLE};
+
+  //picture button
+  document.getElementById('pictureButton').onclick = function() {drawPicture();};
+
+  document.getElementById('randomColorButton').onclick = function() {setRandomColor();};
+
+
+
+
+  //sliders 
+  document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100;});
+  document.getElementById('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/100;});
+  document.getElementById('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/100;});
+
+  //size slider
+  //document.getElementById('sizeSlide').addEventListener('mouseup', function() {g_selectedSize = this.value;});
+
+  //document.getElementById('segmentSlide').addEventListener('mouseup', function() {g_segmentSize = this.value;});
+
+  document.getElementById('angleSlide').addEventListener('mousemove', function() {g_globalAngle = this.value; renderAllShapes();});
+
+  document.getElementById('rightArmSlide').addEventListener('mousemove', function() {g_rightArmAngle = this.value; renderAllShapes();});
+
+  document.getElementById('rightHandSlide').addEventListener('mousemove', function() {g_rightHandAngle = this.value; renderAllShapes();});
+
+  document.getElementById('rightFingerSlide').addEventListener('mousemove', function() {g_rightFingerAngle = this.value; renderAllShapes();});
+
+
+}
+function main() {
+
+  setupWebGL();
+  connectVariablesToGLSL();
+  
+  //connect HTML UI
+  addActionsForHtmlUI();
+
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = click;
+  //canvas.onmousemove = click;
+
+  canvas.onmousemove = function(ev) {if(ev.buttons == 1){click(ev)}};
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+ // gl.clear(gl.COLOR_BUFFER_BIT);
+
+ //renderAllShapes();
+ requestAnimationFrame(tick);
+}
+
+var g_startTime = performance.now()/1000.0;
+var g_seconds = performance.now()/1000.0-g_startTime;
+
+function tick()
+{
+  g_seconds = performance.now()/1000.0-g_startTime;
+
+  //console.log(g_seconds);
+
+  updateAnimationAngles();
+
+  renderAllShapes();
+
+  requestAnimationFrame(tick);
+
+}
+
+
+var g_shapesList = [];
+
+
+
+//var g_points = [];  // The array for the position of a mouse press
+//var g_colors = [];  // The array to store the color of a point
+//var g_sizes = [];
+
+
+function click(ev) {
+  
+  let [x,y] = convertCoordinatesEventToGL(ev);
+
+  let point;
+  if (g_selectedType == POINT)
+  {
+    point = new Point();
+  }
+  else if (g_selectedType == TRIANGLE)
+  {
+    point = new Triangle();
+  }
+  else
+  {
+    point = new Circle();
+    point.segments = g_segmentSize;
+  }
+
+  point.position = [x,y];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapesList.push(point);
+
+ // g_points.push([x, y]);
+
+ // g_colors.push(g_selectedColor.slice());
+
+  //g_sizes.push(g_selectedSize);
+
+  // Store the coordinates to g_points array
+ // if (x >= 0.0 && y >= 0.0) {      // First quadrant
+  //  g_colors.push([1.0, 0.0, 0.0, 1.0]);  // Red
+  //} else if (x < 0.0 && y < 0.0) { // Third quadrant
+  //  g_colors.push([0.0, 1.0, 0.0, 1.0]);  // Green
+  //} else {                         // Others
+   // g_colors.push([1.0, 1.0, 1.0, 1.0]);  // White
+  //}
+
+  renderAllShapes();
+}
+
+function convertCoordinatesEventToGL(ev)
+{
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect();
+
+  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
+
+  // Store the coordinates to g_points array
+  return([x, y]);
+}
+
+function updateAnimationAngles()
+{
+  if(g_armAnimation)
+  {
+    g_rightArmAngle = (-20+45*Math.sin(g_seconds));
+  }
+}
+
+
+function renderAllShapes()
+{
+
+var startTime = performance.now();
+
+var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1, 0);
+gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
+
+
+// Clear <canvas>
+gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+//drawTriangle3D([-1.0, 0.0, 0.0,   -0.5, -1.0, 0.0, 0.0, 0.0, 0.0]);
+
+var body = new Cube();
+body.color = [1.0, 0.0, 0.0, 1.0];
+body.matrix.translate(-.25, -.5, 0.0);
+body.matrix.scale(0.5, 1, .5);
+body.render();
+
+//var duration = performance.now() - startTime;
+
+var rightArm = new Cube();
+rightArm.color = [1,1,0,1];
+rightArm.matrix.setTranslate(.25, 0.15, 0.0);
+rightArm.matrix.rotate(-g_rightArmAngle, 0, 0, 1);
+
+//rightArm.matrix.rotate(45*Math.sin(g_seconds), 0, 0, 1);
+
+rightArm.matrix.scale(0.2, .5, .5);
+/* 
+if(g_armAnimation)
+{
+  rightArm.matrix.rotate(45*Math.sin(g_seconds), 0, 0, 1);
+}
+else
+{
+  rightArm.matrix.rotate(-g_rightArmAngle, 0, 0, 1);
+} */
+
+var handCoordinates = new Matrix4(rightArm.matrix);
+rightArm.render();
+
+var rightHand = new Cube();
+rightHand.color = [0,1,0,1];
+rightHand.matrix = handCoordinates;
+rightHand.matrix.translate(0.3, 1.4, 0.0001);
+rightHand.matrix.rotate(-g_rightHandAngle, 0, 0, 1);
+rightHand.matrix.scale(0.5, 0.5, .5);
+var fingerCoordinates = new Matrix4(rightHand.matrix);
+rightHand.render();
+
+var rightFinger = new Cube();
+rightFinger.color = [1, 0.6, 0, 1]; 
+rightFinger.matrix = fingerCoordinates;
+rightFinger.matrix.translate(-0.3, 0.8, 0.0001); 
+rightFinger.matrix.rotate(-g_rightFingerAngle, 0, 0, 1); 
+rightFinger.matrix.scale(0.4, 0.4, 0.4); 
+rightFinger.render();
+
+
+var leftArm = new Cube();
+leftArm.color = [1,1,0,1];
+leftArm.matrix.setTranslate(-.75, 0.15, 0.0);
+leftArm.matrix.rotate(-90, 0, 0, 1);
+leftArm.matrix.scale(0.2, .5, .5);
+leftArm.render();
+
+var leftHand = new Cube();
+leftHand .color = [0,1,0,1];
+leftHand.matrix.setTranslate(-0.90, 0.125, 0.0);
+leftHand.matrix.rotate(-90, 0, 0, 1);
+leftHand.matrix.scale(0.15, .15, .5);
+leftHand.render();
+
+var head = new Cube();
+head .color = [1,0,1,1];
+head.matrix.setTranslate(-0.1, 0.5, 0.0);
+//head.matrix.rotate(-45, 0, 0, 1);
+head.matrix.scale(0.25, 0.25, .5);
+head.render();
+
+
+var rightLeg = new Cube();
+rightLeg.color = [1,1,0,1];
+rightLeg.matrix.setTranslate(0.05, -0.9, 0.0);
+rightLeg.matrix.rotate(0, 0, 0, 1);
+rightLeg.matrix.scale(0.2, .4, .5);
+rightLeg.render();
+
+var leftLeg = new Cube();
+leftLeg.color = [1,1,0,1];
+leftLeg.matrix.setTranslate(-0.05, -0.9, 0.0);
+leftLeg.matrix.rotate(0, 0, 0, 1);
+leftLeg.matrix.scale(-0.2, .4, .5);
+leftLeg.render();
+
+}
+
+function setTri(x, y, size, color= [1,1,1,1])
+{
+  let tri = new EquilateralTriangle();
+  tri.position = [x, y];
+  tri.size = size;
+  tri.color = color;
+  g_shapesList.push(tri);
+}
+
+
+function drawPicture()
+{
+  //function to draw the picture
+
+  //christmas trees
+
+  setTri(-0.6, 0.2, 30,[0.0, 1.0, 0.0, 1.0] );
+  setTri(-0.6, 0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(-0.6, 0.0, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(-0.6, -0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(-0.6, -0.2, 20, [0.6, 0.3, 0.1, 1]);
+
+  setTri(-0.3, 0.2, 30,[0.0, 1.0, 0.0, 1.0] );
+  setTri(-0.3, 0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(-0.3, 0.0, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(-0.3, -0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(-0.3, -0.2, 20, [0.6, 0.3, 0.1, 1]);
+
+  setTri(0, 0.2, 30,[0.0, 1.0, 0.0, 1.0] );
+  setTri(0, 0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0, 0.0, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0, -0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0, -0.2, 20, [0.6, 0.3, 0.1, 1]);
+
+
+  setTri(0.3, 0.2, 30,[0.0, 1.0, 0.0, 1.0] );
+  setTri(0.3, 0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0.3, 0.0, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0.3, -0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0.3, -0.2, 20, [0.6, 0.3, 0.1, 1]);
+
+  setTri(0.6, 0.2, 30,[0.0, 1.0, 0.0, 1.0] );
+  setTri(0.6, 0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0.6, 0.0, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0.6, -0.1, 30, [0.0, 1.0, 0.0, 1.0]);
+  setTri(0.6, -0.2, 20, [0.6, 0.3, 0.1, 1]);
+
+  
+
+
+  renderAllShapes();
+
+
+}
+
+function setRandomColor()
+{
+  let color1 = Math.random();
+  let color2 = Math.random();
+  let color3 = Math.random();
+
+  g_selectedColor = [color1, color2, color3, 1.0];
+}
