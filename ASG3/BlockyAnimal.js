0888c1efa6742664ecee1c9a7c5c7c674a3683f7
verangarajan <verangar@ucsc.edu>
2025-05-06 18:20:56 -0700

    added texturing

--- ASG3/BlockyAnimal.js
+++ ASG3/BlockyAnimal.js
@@ -24,8 +24,11 @@ var FSHADER_SOURCE = `
   precision mediump float;
   varying vec2 v_UV;
   uniform vec4 u_FragColor;
+  uniform sampler2D u_Sampler0;
   void main() {
     gl_FragColor = u_FragColor;
+    gl_FragColor = vec4(v_UV, 1.0, 1.0);
+    gl_FragColor = texture2D(u_Sampler0, v_UV);
   }
 `
 
@@ -39,6 +42,8 @@ let u_GlobalRotateMatrix;
 let u_ModelMatrix;
 let u_ProjectionMatrix;
 let u_viewMatrix;
+let u_Sampler0;
+
 function setupWebGL()
 {
  // Retrieve <canvas> element
@@ -109,6 +114,13 @@ if (!u_ProjectionMatrix) {
   console.log('Failed to get the storage location of u_ProjectionMatrix');
   return;
 }
+
+  // Get the storage location of u_Sampler
+  u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
+  if (!u_Sampler0) {
+    console.log('Failed to get the storage location of u_Sampler');
+    return false;
+  }
 
 
 var identityM = new Matrix4();
@@ -191,6 +203,51 @@ function addActionsForHtmlUI()
 
 
 }
+
+function initTextures() {
+
+
+  var image = new Image();  // Create the image object
+  if (!image) {
+    console.log('Failed to create the image object');
+    return false;
+  }
+  // Register the event handler to be called on loading an image
+  image.onload = function(){ sendTextureToTEXTURE0(image); };
+  // Tell the browser to load an image
+  image.src = 'sky.jpg';
+
+  return true;
+}
+
+function sendTextureToTEXTURE0(image) {
+
+  var texture = gl.createTexture();   // Create a texture object
+  if (!texture) {
+    console.log('Failed to create the texture object');
+    return false;
+  }
+
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+  // Enable texture unit0
+  gl.activeTexture(gl.TEXTURE0);
+  // Bind the texture object to the target
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the texture parameters
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // Set the texture image
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  
+  // Set the texture unit 0 to the sampler
+  gl.uniform1i(u_Sampler0, 0);
+  
+ // gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+ // gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+}
+
+
 function main() {
 
   setupWebGL();
@@ -205,6 +262,9 @@ function main() {
 
   canvas.onmousemove = function(ev) {if(ev.buttons == 1){click(ev)}};
   // Specify the color for clearing <canvas>
+
+  initTextures();
+
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
--- ASG3/Cube.js
+++ ASG3/Cube.js
@@ -25,7 +25,9 @@ class Cube
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements)
 
-   drawTriangle3DUV([0,0,0, 1,1,0, 1,0,0], [1,0,0,1,1,1]);
+   drawTriangle3DUV([0,0,0, 1,1,0, 1,0,0], [0,0,1,1,1,0]);
+   drawTriangle3DUV([0,0,0, 0,1,0, 1,1,0], [0,0,0,1,1,1]);
+
 
   
     //Pass the size to u_Size
