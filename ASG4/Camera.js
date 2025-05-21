5138f4369006959f3c149a72d9441299972bb529
verangarajan <verangar@ucsc.edu>
2025-05-21 12:47:34 -0700

    initial commit

---
+++ ASG4/Camera.js
@@ -0,0 +1,95 @@
+class Camera
+{
+    constructor()
+    {
+        this.eye = new Vector3([0, 0,3]);
+        this.at = new Vector3([0, 0, -100]);
+        this.up = new Vector3([0, 1, 0]);
+    }
+
+    forward()
+    {
+        var f = this.at.sub(this.eye);
+       // console.log("This is f", f);
+        f=f.div(f.magnitude());
+        this.at = this.at.add(f);
+       // console.log("This is at", this.at)
+        this.eye = this.eye.add(f);
+        //console.log("This is eye", this.eye)
+    }
+
+    back()
+    {
+        var f = this.eye.sub(this.at);
+        f = f.div(f.magnitude());
+        this.at = this.at.add(f);
+        this.eye = this.eye.add(f);
+    }
+
+    left()
+    {
+        var f = this.eye.sub(this.at);
+        f = f.div(f.magnitude());
+        var s = Vector3.cross(f, this.up);
+        s=s.div(s.magnitude());
+        this.at = this.at.add(s);
+        this.eye = this.eye.add(s);
+    }
+
+    right() {
+        var f = this.eye.sub(this.at);
+        f = f.div(f.magnitude()); 
+        var s = Vector3.cross(f, this.up);       
+        s = s.div(s.magnitude());
+        this.at = this.at.sub(s);
+        this.eye = this.eye.sub(s);
+    }
+
+    panLeft() {
+        const angle = 2; // degrees
+        const dir = new Vector3(this.at.elements); // clone `at`
+        dir.sub(this.eye); // direction vector from eye to at
+    
+        const rotation = new Matrix4().setRotate(angle, this.up.elements[0], this.up.elements[1], this.up.elements[2]);
+        const newDir = rotation.multiplyVector3(dir); // rotate the direction
+    
+        this.at = new Vector3(this.eye.elements); // reset at to eye
+        this.at.add(newDir); // add rotated direction to eye
+    }
+    
+    panRight() {
+        const angle = -2; // negative for right
+        const dir = new Vector3(this.at.elements); // clone `at`
+        dir.sub(this.eye); // direction vector from eye to at
+    
+        const rotation = new Matrix4().setRotate(angle, this.up.elements[0], this.up.elements[1], this.up.elements[2]);
+        const newDir = rotation.multiplyVector3(dir); // rotate the direction
+    
+        this.at = new Vector3(this.eye.elements); // reset at to eye
+        this.at.add(newDir); // add rotated direction to eye
+    }
+
+    panLeftDrag() {
+        const angle = 20; // degrees
+        const dir = new Vector3(this.at.elements); // clone `at`
+        dir.sub(this.eye); // direction vector from eye to at
+    
+        const rotation = new Matrix4().setRotate(angle, this.up.elements[0], this.up.elements[1], this.up.elements[2]);
+        const newDir = rotation.multiplyVector3(dir); // rotate the direction
+    
+        this.at = new Vector3(this.eye.elements); // reset at to eye
+        this.at.add(newDir); // add rotated direction to eye
+    }
+    
+    panRightDrag() {
+        const angle = -20; // negative for right
+        const dir = new Vector3(this.at.elements); // clone `at`
+        dir.sub(this.eye); // direction vector from eye to at
+    
+        const rotation = new Matrix4().setRotate(angle, this.up.elements[0], this.up.elements[1], this.up.elements[2]);
+        const newDir = rotation.multiplyVector3(dir); // rotate the direction
+    
+        this.at = new Vector3(this.eye.elements); // reset at to eye
+        this.at.add(newDir); // add rotated direction to eye
+    }
+}
---
+++ ASG4/Cube.js
@@ -0,0 +1,78 @@
+class Cube
+{
+    constructor()
+    {
+        this.type = "cube";
+       // this.position = [0.0, 0.0, 0.0];
+        this.color = [1,0, 1.0, 1.0, 1.0];
+      //  this.size = 5.0;
+      //  this.segments = 10;
+
+      this.matrix = new Matrix4();
+      this.textureNum = -2;
+
+    }
+
+    render()
+  {
+//    var xy = this.position;
+    var rgba = this.color;
+   // var size = this.size;
+
+   gl.uniform1i(u_whichTexture, this.textureNum);
+  
+  
+    // Pass the position of a point to a_Position variable
+   // gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
+    // Pass the color of a point to u_FragColor variable
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements)
+
+   drawTriangle3DUV([0,0,0, 1,1,0, 1,0,0], [0,0,1,1,1,0]);
+   drawTriangle3DUV([0,0,0, 0,1,0, 1,1,0], [0,0,0,1,1,1]);
+
+
+  
+    //Pass the size to u_Size
+    //gl.uniform1f(u_Size, size);
+    // Draw
+
+    drawTriangle3D([0,0,0,  1, 1, 0, 1, 0, 0]);
+    drawTriangle3D([0,0,0, 0,1,0, 1, 1, 0]);
+
+    gl.uniform4f(u_FragColor, rgba[0] * .9, rgba[1]* .9, rgba[2]* .9, rgba[3]);
+
+
+    drawTriangle3D([0, 1, 0,   0, 1, 1 , 1,1,1 ]);
+    drawTriangle3D([0,1,0  , 1,1,1,   1,1,0]);
+
+     // Top face
+     gl.uniform4f(u_FragColor, rgba[0] * 0.8, rgba[1] * 0.8, rgba[2] * 0.8, rgba[3]);
+     drawTriangle3D([0, 1, 0,   0, 1, 1,   1, 1, 1]);
+     drawTriangle3D([0, 1, 0,   1, 1, 1,   1, 1, 0]);
+ 
+     // Bottom face
+     gl.uniform4f(u_FragColor, rgba[0] * 0.7, rgba[1] * 0.7, rgba[2] * 0.7, rgba[3]);
+     drawTriangle3D([0, 0, 0,   1, 0, 0,   1, 0, 1]);
+     drawTriangle3D([0, 0, 0,   1, 0, 1,   0, 0, 1]);
+ 
+     // Right face
+     gl.uniform4f(u_FragColor, rgba[0] * 0.6, rgba[1] * 0.6, rgba[2] * 0.6, rgba[3]);
+     drawTriangle3D([1, 0, 0,   1, 1, 0,   1, 1, 1]);
+     drawTriangle3D([1, 0, 0,   1, 1, 1,   1, 0, 1]);
+ 
+     // Left face
+     gl.uniform4f(u_FragColor, rgba[0] * 0.5, rgba[1] * 0.5, rgba[2] * 0.5, rgba[3]);
+     drawTriangle3D([0, 0, 0,   0, 0, 1,   0, 1, 1]);
+     drawTriangle3D([0, 0, 0,   0, 1, 1,   0, 1, 0]);
+
+     gl.uniform4f(u_FragColor, rgba[0] * 0.4, rgba[1] * 0.4, rgba[2] * 0.4, rgba[3]);
+    drawTriangle3D([0, 0, 1,   1, 0, 1,   1, 1, 1]);
+    drawTriangle3D([0, 0, 1,   1, 1, 1,   0, 1, 1]);
+  
+  }
+
+
+  
+
+}
